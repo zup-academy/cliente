335efe73faba7e758d5109f3a44a39b07ceb8072
paula.santana <paula.santana@zup.com.br>
2022-08-04 10:56:03 -0300

    Adicionando ajustes

---
+++ src/main/java/com/zcoders/cliente/error/ExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.zcoders.cliente.error;
+
+import com.zcoders.cliente.exception.ClienteExistenteException;
+import com.zcoders.cliente.exception.ClienteInexistenteException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeralErrors(RuntimeException e, WebRequest request) {
+
+        logger.error("Exception : " + e.getLocalizedMessage(), e);
+
+        Map<String, Object> body = Map.of(
+                "status", 500,
+                "path", request.getDescription(false).replace("uri=", ""),
+                "timestamp", LocalDateTime.now(),
+                "message", e.getLocalizedMessage()
+        );
+        return ResponseEntity
+                .internalServerError().body(body);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({ClienteInexistenteException.class,
+    ClienteExistenteException.class})
+    public ResponseEntity<?> handleError(RuntimeException e, WebRequest request) {
+
+        logger.error("Exception : " + e.getLocalizedMessage(), e);
+
+        Map<String, Object> body = Map.of(
+                "status", 400,
+                "path", request.getDescription(false).replace("uri=", ""),
+                "timestamp", LocalDateTime.now(),
+                "message", e.getLocalizedMessage()
+        );
+        return ResponseEntity
+                .badRequest().body(body);
+    }
+
+}
