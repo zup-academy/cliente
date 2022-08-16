c1db4f77398d44445630d1c359a463a03d7dd86e
paula.santana <paula.santana@zup.com.br>
2022-08-16 16:33:35 -0300

    Ajustes para o workshop

--- src/main/java/com/zcoders/cliente/response/ClienteResponse.java
+++ src/main/java/com/zcoders/cliente/response/ClienteResponse.java
@@ -2,9 +2,7 @@ package com.zcoders.cliente.response;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.zcoders.cliente.model.Cliente;
